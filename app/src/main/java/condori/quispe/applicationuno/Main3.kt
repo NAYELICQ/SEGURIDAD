eb813903243093277747dbaaad86b4a856274181
lenovo <lenovo@gmail.com>
2024-06-02 00:19:23 -0500

    primer

---
+++ app/src/main/java/condori/quispe/applicationuno/Main3.kt
@@ -0,0 +1,28 @@
+package condori.quispe.applicationuno
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Main3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main3)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val button=findViewById<Button>(R.id.SIGUIENTE)
+
+        button.setOnClickListener {
+            val intent = Intent(this, Main4::class.java)
+            startActivity(intent)
+        }
+    }
+}
